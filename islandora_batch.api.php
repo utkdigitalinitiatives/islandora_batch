f1c574098f6678f2762a0ab290d2bc9d7d8535a3
Matthew Perry <mperry@discoverygarden.ca>
2015-02-24 14:06:48 -0400

    Before hook is called build an array of successfully processed sets and pass that instead of results to the hook function.

--- islandora_batch.api.php
+++ islandora_batch.api.php
@@ -8,9 +8,17 @@
 /**
  * A hook to allow for events directly after a islandora batch has finished.
  *
- * @param array $results
- *   The array of result data passed into the finish function. Note that when
- *   processing a single set it contains the set id stored in ingest_set.
+ * @param array $sets
+ *   An array of batch set ids that have successfully ingested all objects at
+ *   the end of batch processing.
  */
-function hook_islandora_batch_process_finished($results) {
+function hook_islandora_batch_process_finished($sets) {
+  if (!empty($sets)) {
+    module_load_include('inc', 'islandora_batch', 'includes/db');
+    foreach ($sets as $set) {
+      // Do something to the set, in this example it is completely deleting it
+      // from the drupal database.
+      islandora_batch_delete_set($set);
+    }
+  }
 }
